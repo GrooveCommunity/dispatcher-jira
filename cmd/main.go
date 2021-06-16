736356c3bc1d6d46e83acf5528f3857cb4433751
Eduardo Dicarte <eduardo.pereira@groove.tech>
2021-06-16 19:48:00 -0300

    Recuperando endereço do webhook

--- cmd/main.go
+++ cmd/main.go
@@ -19,7 +19,6 @@ func main() {
 	fmt.Println("Port: ", os.Getenv("APP_PORT"))
 
 	panic(http.ListenAndServe(":"+os.Getenv("APP_PORT"), router))
-	//log.Fatal(http.ListenAndServe(":"+os.Getenv("APP_PORT"), router))
 }
 
 func handleValidateHealthy(w http.ResponseWriter, r *http.Request) {
@@ -27,14 +26,15 @@ func handleValidateHealthy(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleQueueDispatcher(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Recebendo fila")
-
 	var dispatcherRequest interface{}
 
 	if err := json.NewDecoder(r.Body).Decode(&dispatcherRequest); err != nil {
 		http.Error(w, fmt.Sprintf("Não foi possível decodificar o body: %v", err), http.StatusBadRequest)
 		return
 	}
+
+	fmt.Println("Address: " + r.Header.Get("X-FORWARDED-FOR"))
+	fmt.Println("Remote ADDR: " + r.RemoteAddr)
 
 	fmt.Println(dispatcherRequest)
 }
