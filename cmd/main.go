0b187b4dca076e73acef30f1704b0a9c1d749150
Eduardo Dicarte <eduardo.pereira@groove.tech>
2021-06-19 19:21:32 -0300

    Decodificando mensagem da fila

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/GrooveCommunity/dispatcher-jira/entity"
+	"google.golang.org/api/pubsub/v1"
 
 	"github.com/gorilla/mux"
 )
+
+type pushRequest struct {
+	Message      pubsub.PubsubMessage
+	Subscription string
+}
 
 func main() {
 	router := mux.NewRouter()
@@ -29,9 +35,20 @@ func handleValidateHealthy(w http.ResponseWriter, r *http.Request) {
 
 func handleQueueDispatcher(w http.ResponseWriter, r *http.Request) {
 
-	body, _ := ioutil.ReadAll(r.Body)
+	var pr pushRequest
 
-	log.Println(string(body))
+	if err := json.NewDecoder(r.Body).Decode(&pr); err != nil {
+		http.Error(w, fmt.Sprintf("Não foi possível decodificar o body"), http.StatusBadRequest)
+		return
+	}
+
+	requestJson, err := base64.StdEncoding.DecodeString(pr.Message.Data)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Não foi possível decodificar o json da fila"), http.StatusBadRequest)
+		return
+	}
+
+	log.Println(string(requestJson))
 
 	/*var dispatcherRequest interface{}
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/GrooveCommunity/dispatcher-jira
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	google.golang.org/api v0.48.0 // indirect
+)
